9b1ae3f7d82925db1199d186acc5a87a6d8455e4
karnse <suphawit.se@ku.th>
2023-02-07 11:42:40 +0700

    update-loker

--- router/locker.py
+++ router/locker.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 from datetime import datetime
-
+from fastapi import APIRouter
 class Container(BaseModel):
     item_in_contain:str
     minute:int
     locker_id:int
     uid:int
-    check_in_time:datetime=datetime(0)
-    check_out_time:datetime=datetime(0)
+    check_in_time:datetime=None
+    check_out_time:datetime=None
     cost:int = 0
+
+router = APIRouter(prefix='/locker')
